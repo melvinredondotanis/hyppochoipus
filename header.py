16b3da0080f0ebf098b8cfd0931820bab0b37727
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-15 19:10:37 +0200

    Refactor main.py to use faster_whisper for language detection and transcription

--- header.py
+++ header.py
@@ -20,5 +20,58 @@ def welcome():
     system("clear")
 
 
+def settings():
+    """
+    This function displays the settings menu
+    and waits for the user to select a model, language, and number of students.
+
+    Returns:
+        tuple: The selected model, language, and number of students
+    """
+
+    models = [
+        "tiny",
+        "small",
+        "medium",
+        "large",
+        "huge"
+    ]
+
+    languages = [
+        "en",
+        "fr"
+    ]
+
+    for i, model in enumerate(models, start=0):
+        print("[{}] {}".format(i, model))
+
+    try:
+        model_number = int(input("Select a model [number]: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+
+    for i, language in enumerate(languages, start=0):
+        print("[{}] {}".format(i, language))
+
+    try:
+        language_number = int(input("Select a language [number]: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+
+    try:
+        students_number = int(input(
+                                    "Select the number of students [number]: "
+                                    ))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+
+    system("clear")
+    return models[model_number], languages[language_number], students_number
+
+
 if __name__ == "__main__":
     welcome()
+    settings()
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 from os import remove
-from header import welcome
+from header import welcome, settings
 from speech_to_text import speech_to_text
 from ollama import chat
 
 def main():
+    model, language, students = settings()
     messages = []
-    while True:
-        user_input = speech_to_text("tiny", "fr")
+    while students > 0:
+        user_input = speech_to_text(model, language)
         if not user_input:
             exit()
         print()
@@ -16,6 +17,7 @@ def main():
         message = chat(messages)
         messages.append(message)
         print("\n\n")
+        students -= 1
 
 if __name__ == "__main__":
     try:
@@ -25,4 +27,7 @@ if __name__ == "__main__":
         print("\nProgram terminated by user")
     finally:
         print("Cleaning up...")
-        remove("audio.wav")
+        try:
+            remove("audio.wav")
+        except FileNotFoundError:
+            pass
